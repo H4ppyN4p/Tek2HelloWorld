f076b1003b0707a069ac6c209ac769b53f9b4edb
H4ppyN4p <tobi5703@stud.kea.dk>
2022-08-23 09:21:46 +0200

    Test

---
+++ src/test/java/com/example/helloworldazure/HelloWorldAzureApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.helloworldazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAzureApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
